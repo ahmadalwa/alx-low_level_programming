dd9622dad53e33ad3534c0142b99e1643a593e66
ahmadalwa <dr.ahmadalwa@gmail.com>
2023-12-13 03:50:11 -0800

    last2

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp libwin.so https://github.com/ALX-SE-projects/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libwin.so
+wget -P /tmp libwin.so https://github.com/ahmadalwa/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libwin.so
 export LD_PRELOAD=/tmp/libwin.so
